e4d0c0b88960148df5f32eb6b5e981a65a9a17d9
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-19 16:05:59 -0600

    Build forecast search facade to facilitate api calls

---
+++ app/facades/forecast_search.rb
@@ -0,0 +1,16 @@
+class ForecastSearch
+  attr_reader :forecast
+
+  def initialize(location)
+    @location = location
+    @forecast = get_forecast
+  end
+
+  def get_forecast
+    location_data = Location.new(@location)
+    weather = Forecast.new
+    weather = weather.by_location(location_data)
+
+    location_forecast = LocationForecast.new(@location, weather)
+  end
+end
