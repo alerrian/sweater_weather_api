df08ca9ef7be9cc1ec00944e0127415c9c361aa9
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-19 16:59:30 -0600

    Add background search class to facilitate the movement from controller to service

---
+++ app/facades/background_search.rb
@@ -0,0 +1,13 @@
+class BackgroundSearch
+  def initialize(location)
+    @location = location
+    @background = get_background
+  end
+
+  def get_background
+    service = PexelsService.new
+    background = service.background_data(@location)
+
+    LocationBackground.new(@location, background)
+  end
+end
