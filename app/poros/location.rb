1773adfa3859beede5053928e387588bcfd6fb82
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-19 16:05:32 -0600

    Fix location poro

--- app/poros/location.rb
+++ app/poros/location.rb
@@ -14,8 +14,6 @@ class Location
     data = GoogleService.new
     location_data = data.location_data(location)
 
-    json = JSON.parse(location_data.body, symbolize_names: true)
-
-    json[:results].first[:geometry][:location]
+    location_data[:results].first[:geometry][:location]
   end
 end
