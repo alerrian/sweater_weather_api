6a8bc580ae30c2c5c146c93ac51b78891e15ad54
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-19 16:56:48 -0600

    Fixed google service to properly abstract key

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,13 +1,14 @@
 class GoogleService
   def location_data(location)
     response = connection.get('/maps/api/geocode/json') do |faraday|
-      faraday.params['key'] = ENV['GGEO_KEY']
       faraday.params['components'] = "locality:#{location}"
     end
     JSON.parse(response.body, symbolize_names: true)
   end
-
+  
   def connection
-    Faraday.new(url: 'https://maps.googleapis.com')
+    Faraday.new(url: 'https://maps.googleapis.com') do |faraday|
+      faraday.params['key'] = ENV['GGEO_KEY']
+    end
   end
 end
