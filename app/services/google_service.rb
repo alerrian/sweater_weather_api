6961831d5df3633d3e98abdc0203afe2e58c7b09
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-19 16:07:09 -0600

    Refactor services to be more intuitive and handle data more appropriately

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,9 +1,10 @@
 class GoogleService
   def location_data(location)
-    connection.get('/maps/api/geocode/json') do |faraday|
+    response = connection.get('/maps/api/geocode/json') do |faraday|
       faraday.params['key'] = ENV['GGEO_KEY']
       faraday.params['components'] = "locality:#{location}"
     end
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def connection
--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,15 +1,19 @@
 class OpenWeatherService
   def weather_data(lat, long)
-    connection.get('/data/2.5/onecall') do |faraday|
+    response = connection.get('/data/2.5/onecall') do |faraday|
       faraday.params['lat'] = lat
       faraday.params['lon'] = long
-      faraday.params['appid'] = ENV['OWM_KEY']
+      faraday.params['units'] = 'imperial'
     end
+
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   private
 
   def connection
-    Faraday.new(url: 'https://api.openweathermap.org')
+    Faraday.new(url: 'https://api.openweathermap.org') do |faraday|
+      faraday.params['appid'] = ENV['OWM_KEY']
+    end
   end
 end
