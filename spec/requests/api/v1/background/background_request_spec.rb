b722f477335044c3cf0f0c67e1fd35142de5badb
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-19 16:56:01 -0600

    Added test to check the api/v1/backgrounds endpoint pointing to denver

---
+++ spec/requests/api/v1/background/background_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast API', type: :request do
+  it 'should be able to retrieve a forecast for a specific city', :vcr do
+    location = 'Denver, CO'
+
+    get "/api/v1/backgrounds?location=#{location}"
+
+    expect(response).to be_successful
+  end
+end
