4c23b43c5181f29cc47de6df840002c48c81962d
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-20 14:48:58 -0600

    Add google service connection.get for destinations

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -6,6 +6,14 @@ class GoogleService
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  def trip_data(origin, destination)
+    response = connection.get('/maps/api/directions/json') do |faraday|
+      faraday.params['origin'] = origin
+      faraday.params['destination'] = destination
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   private
   
   def connection
