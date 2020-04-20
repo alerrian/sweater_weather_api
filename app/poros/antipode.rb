5963fb89b8177be4a9b6bce00c58cc6c36de23f0
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-20 09:32:37 -0600

    Finalize poro for antipode

--- app/poros/antipode.rb
+++ app/poros/antipode.rb
@@ -2,13 +2,13 @@ class Antipode
   def initialize(lat, long)
     @lat = lat
     @long = long
-    @antipode_lat = get_antipode
-    @antipode_long = get_antipode
+    @antipode_lat = get_antipode[:lat]
+    @antipode_long = get_antipode[:long]
   end
 
   def get_antipode
     service = AntipodeService.new
     antipode_data = service.antipode_location(@lat, @long)
-    require 'pry'; binding.pry
+    antipode_data[:data][:attributes]
   end
 end
