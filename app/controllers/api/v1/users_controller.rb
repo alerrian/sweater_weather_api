e85849551bb46a0a95a537ad124d4a647c4d9af0
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-19 20:10:48 -0600

    Added user create action to controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: UsersSerializer.new(@user)
+    else
+      render status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:users).permit(:email, :password, :password_confirmation)
+  end
+end
