0593fb93d657b6928c8dd527138758c7a0598ca4
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-04-20 09:29:58 -0600

    Add service to make outer call ro amypode

---
+++ app/services/antipode_service.rb
@@ -0,0 +1,18 @@
+class AntipodeService
+  def antipode_location(lat, long)
+    require 'pry'; binding.pry
+    response = connection.get('/api/v1/antipodes') do |faraday|
+      faraday.params['lat'] = lat
+      faraday.params['long'] = long
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+
+  end
+
+  def connection
+    Faraday.new(url: 'http://amypode.herokuapp.com') do |faraday|
+      faraday.headers['api_key'] = ENV['AMYPODE_KEY']
+    end
+  end
+end
